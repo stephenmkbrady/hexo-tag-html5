9aea010b9bdcbaa9ff48ca10d8fcc87ba3c06853
Stephen Brady <Stephen Brady>
2016-10-24 20:08:54 +0100

    fixed args position issue

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var pathFn = require('path');
 
 hexo.extend.tag.register('html5video', function(args, content){
   var filename = content;  
-  var width = args[1] || '100%',
-      height = args[2] || '250px',
-      codec = args[3] || 'video/webm';
+  var width = args[0] || '100%',
+      height = args[1] || '250px',
+      codec = args[2] || 'video/webm';
      
   return "<video width=" + "'" + width + "'" + " height=" + "'" + height + "'" + " controls>" + "<source src=" + "'" + filename + "'" + " type=" + codec +">"+" Your browser does not support the webm tag."+"</video>";
 }, {ends: true});
